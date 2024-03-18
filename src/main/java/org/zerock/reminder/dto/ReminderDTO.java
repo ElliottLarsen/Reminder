83f5f3537967f060fbeb8177266998047d8e6a11
Elliott Larsen <elliottlrsn@gmail.com>
2024-03-17 20:03:34 -0600

    DTO added.

--- src/main/java/org/zerock/reminder/dto/ReminderDTO.java
+++ src/main/java/org/zerock/reminder/dto/ReminderDTO.java
@@ -1,4 +1,22 @@
 package org.zerock.reminder.dto;
 
+import jdk.internal.vm.annotation.IntrinsicCandidate;
+import jdk.vm.ci.meta.Local;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReminderDTO {
+    private long id;
+    private String reminder;
+    private LocalDate createDate;
+    private LocalDate dueDate;
+    private boolean completed;
 }
