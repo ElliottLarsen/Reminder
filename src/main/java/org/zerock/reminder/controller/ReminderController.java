ef3382cc6a95accab102e63b2716d3d91e8d780c
Elliott Larsen <elliottlrsn@gmail.com>
2024-03-23 20:08:21 -0600

    Controller doGet().

--- src/main/java/org/zerock/reminder/controller/ReminderController.java
+++ src/main/java/org/zerock/reminder/controller/ReminderController.java
@@ -1,6 +1,7 @@
 package org.zerock.reminder.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.zerock.reminder.dto.ReminderDTO;
 import org.zerock.reminder.service.ReminderService;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "reminderController", value="/reminder/list")
 @Log4j2
@@ -19,5 +21,13 @@ public class ReminderController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("REMINDER LIST--------------------");
+        try {
+            List<ReminderDTO> dtoList = reminderService.listALL();
+            req.setAttribute("dtoList", dtoList);
+            req.getRequestDispatcher("/WEB-INF/reminder/list.jsp").forward(req, resp);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            throw new ServletException("list error");
+        }
     }
 }
